b7d7838c14dc02db8408e00b04e368c6bb502b46
BhavyaBh289 <shahbhavyasp@gmail.com>
2024-02-06 16:12:36 +0530

    array based josephus done

--- lab_5/array.c
+++ lab_5/array.c
@@ -2,23 +2,26 @@
 void main (){
     int n;
     scanf("%d",&n);
-    int array[n];
+    int arr[n];
+    int i, position = 0;
+
+    for (i = 0; i < n; i++) {
+        arr[i] = 1;
+    }
+
+    for (i = 0; i < n - 1; i++) {
+        int count = 0;
+        while (count < 2) {
+            position = (position + 1) % n;
+            if (arr[position] == 1) {
+                count++;
+            }
+        }
+        arr[position] = 0;
+    }
     for (int i = 0; i < n; i++) {
-        array[i] = 1;
-    }
-    int holder = 0;
-    int next = 1;
-    while(array[holder]!=0){
-        printf("%d  ",next);
-        array[next]=0;
-        while(array[next]!=1){
-            next=(next+1)%n;
-        }
-        holder=next;
-        next=(next+1)%n;
-        while(array[next]!=1){
-            next=(next+1)%n;
+        if(arr[i]==1){
+            printf("%d ", i);
         }
     }
-    printf("%d",holder);
 }
